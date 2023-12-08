91529c0fadd137c95415753b7f12c2efbf9a2549
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-08 14:37:08 +0100

    classlist 2.1

---
+++ assets/js/main_classlist_2_1.js
@@ -0,0 +1,10 @@
+function changeHeadline() {
+    let firstButton = document.querySelector("#headline")
+    firstButton.classList.add("headline")
+}
+
+function reset() {
+    let secondButton = document.querySelector("#headline")
+    secondButton.classList.remove("headline")
+}
+
