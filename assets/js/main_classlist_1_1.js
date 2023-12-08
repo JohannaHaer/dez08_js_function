e5a80eb7f90165668f298c9378e6dd3b446cd709
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-08 14:24:18 +0100

    classlist 1.1

---
+++ assets/js/main_classlist_1_1.js
@@ -0,0 +1,7 @@
+function addStyle() {
+    let headline = document.querySelector("#headline")
+    headline.classList.add("headline")
+    let list = document.querySelector("#list")
+    list.classList.add("list")
+}
+
